fbbdc864d347492ad197842bca74218550b09c81
ldierker <lauren.dierker@tufts.edu>
2015-10-24 04:02:41 -0400

    added spotify api requests to grab user id and create playlist

--- make-playlist.js
+++ make-playlist.js
@@ -43,12 +43,17 @@
 		//open spotify
 
 		Query.ajaxSettings.traditional = true; 
+
+		//my api key
 		var apiKey = ZSB4FV0A9YETOVCG1;
+		//url to request songs
+		//TODO may change metrics used later
     	var url = 'http://developer.echonest.com/api/v4/song/search?api_key=' + apiKey + 
     	          '&format=json&results=' + numSongs + '&min_danceability=' + min_danceability +
     	          '&max_danceability=' + max_danceability + '&min_energy=' + min_energy + 
     	          '&max_energy=' + max_energy + '&min_hotttnesss=' + min_hotttnessss;
     	          
+    	//http get from api 
     	var response = new XMLHttpRequest();
     	response.open("GET", url, false);          
     	var parsedJSON = response.parse(response);
@@ -80,5 +85,46 @@
                 error("Trouble getting blog posts for " + artist);
             }
         );
-	*/		
+	*/
+	//get the current spotify users id 
+
+	
+
+	}
+
+	/*
+	 * PURPOSE: creates a new spotify playlist for client. List of songs from echo nest
+	 *          will be added 
+	 */
+	function createSpotifyPlaylist () {
+		var currentUserId = getUserInfo(['id']);
+		var playlistName = "foobar"
+		var url = 'https://api.spotify.com/v1/users/' + currentUserId + '{user_id}/playlists?' 
+				   + 'name=' + playlistName + '&public=true';
+		POST 
+	}
+	
+
+	/*
+	 * PURPOSE: gets spotify user information of client. 
+	 *  RETURN: currentUserInfo - JSON data of user 
+	 */
+	function getUserInfo () {
+		var apiKeySpotify = "ea7eb5c1d5514aafb06d0c2494fc1fc8"
+		var currentUserInfo = new XMLHttpRequest();
+		var url = 'https://api.spotify.com/v1/me?client_id=' + apiKeySpotify;
+		currentUserInfo.open("GET", url)
+
+		callbackFunction;
+
+		currentUserInfo.send();		
+	
+		return currentUserInfo;
+	}
+
+	function callbackFunction (request) {
+			if (request.readyState == 4 && request.status == 200) {
+					alert ("Got Spotify User data back");
+
+			}
 	}
